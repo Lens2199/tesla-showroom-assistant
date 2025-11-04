2cde735f3b3968768df8752820feb6587d1717c5
Deo Cherisme <deolenscherisme1000@gmail.com>
2025-11-04 12:30:33 -0500

    Added user input for Tesla model selection

---
+++ index.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+console.log ("welcome to the tesla showroom assistant!");
+
+let price = 0;
+
+let userInput = prompt("Which Tesla model are you interested in? (Model 3 / Model Y / Model S):");
+
+
+
